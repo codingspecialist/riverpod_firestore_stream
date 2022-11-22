5cd6fb1cbba851a00477cece4bb5476499d47b8d
ssar <ssar@nate.com>
2022-11-22 17:36:41 +0900

    autodispose

--- lib/domain/chat/chat_firestore_repository.dart
+++ lib/domain/chat/chat_firestore_repository.dart
@@ -4,7 +4,8 @@ import 'package:riverpod_firestore_stream/domain/chat/chat.dart';
 import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
 
 // 리턴 : Stream<List<Chat>>
-final chatStreamProvider = StreamProvider<List<Chat>>((ref) {
+final chatStreamProvider = StreamProvider.autoDispose<List<Chat>>((ref) {
+  //ref.onDispose(() { });
   final db = FirebaseFirestore.instance;
   Stream<QuerySnapshot<Map<String, dynamic>>> stream = db.collection("chat").snapshots();
   return stream.map((snapshot) => snapshot.docs.map((doc) => Chat.fromJson(doc.data())).toList());
